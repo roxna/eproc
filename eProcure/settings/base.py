f2bc23e63172da1a76ff3db938941d8b0281cfdc
ROXNA <roxna.irani@gmail.com>
2017-03-27 13:20:40 +0530

    Whitenoise to serve staticfiles on heroku

--- eProcure/settings/base.py
+++ eProcure/settings/base.py
@@ -54,6 +54,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',    
+    'whitenoise.middleware.WhiteNoiseMiddleware',  #To serve static files on heroku
 )
 
 ROOT_URLCONF = 'eProcure.urls'
--- eProcure/urls.py
+++ eProcure/urls.py
@@ -163,6 +163,10 @@ urlpatterns=[
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
 # if settings.DEBUG:
 #     # static files (images, css, javascript, etc.)
 #     urlpatterns += patterns('',
